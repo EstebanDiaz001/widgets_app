9c55ec094bd31e4bb6b80b88ba4d2607e0f6a56c
Esteban <202331e@gmail.com>
2023-05-08 15:35:53 -0500

    go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +8,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Flutter + material 3'),
       ),
@@ -20,7 +19,6 @@ class HomeScreen extends StatelessWidget {
 class _HomeView extends StatelessWidget {
   const _HomeView();
 
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -30,7 +28,7 @@ class _HomeView extends StatelessWidget {
 
         return _CustomListTile(menuItem: menuItem);
       },
-      );
+    );
   }
 }
 
@@ -43,16 +41,29 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
-
     return ListTile(
-      leading: Icon(menuItem.icon, color: colors.primary ),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: colors.primary,
+      ),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: () => {},
+      onTap: ()  {
+
+        
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // )
+        // Navigator.pushNamed(context, menuItem.link)
+        context.push(menuItem.link);
+
+
+      },
     );
   }
 }
